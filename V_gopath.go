5ce921a105747e8f82c5bc0cee05a410ce020c8e
congxia wu <1398173038@qq.com>
2023-11-10 18:58:41 +0800

    v0.0.0

---
+++ V_gopath.go
@@ -0,0 +1,16 @@
+package V_gopath
+
+import "C"
+import (
+	"fmt"
+	"github.com/deer7777/C"
+)
+
+/*func main()  {
+	V_print()
+}*/
+
+func V_print() {
+	fmt.Println("version V_v0.0.0")
+	C.C_print()
+}
